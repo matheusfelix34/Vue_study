ca79064103e98d504ad9fb79ac39b21ab5f169eb
Matheus Felix <matheus.felix@prodeb.ba.gov.br>
2025-07-22 11:22:30 -0300

    praticando v-for

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="header">
+        Header
+    </div>
+</template>
+
+
+<script setup>
+    
+</script>
+
+<style>
+    .header {
+       background: #000;
+       color: #fff; 
+    }
+</style>
