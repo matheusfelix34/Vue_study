e21cd13f960c18571cf811a1598991fa89166f36
Matheus Felix <matheus.felix@prodeb.ba.gov.br>
2025-08-04 10:58:54 -0300

    feat(src/components/BaseCard.vue): adicionando componente BaseCard com style scoped

---
+++ app/src/components/BaseCard.vue
@@ -0,0 +1,17 @@
+<template>
+ <div class="card">
+   Lore ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Ex
+cepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    
+ </div>
+</template>
+
+<style scoped>
+.card {
+  background: bisque;
+  padding: 10px;
+  border-radius: 5px;
+  margin: 10px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+}
+</style>
